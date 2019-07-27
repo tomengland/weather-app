5fe4b289ff9e0194d9b5a2d59bb16d95f15fa927
THOMAS ENGLAND <thomasengland@THOMASs-MacBook-Pro.local>
2019-07-27 07:25:21 -1000

    remove whitespace

--- public/js/app.js
+++ public/js/app.js
@@ -3,7 +3,6 @@ const searchText = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
 
-
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
     const location = searchText.value
